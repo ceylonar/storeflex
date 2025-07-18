29503bea9e830700fe344e2762dacb55a7cf6111
Ceylon Ar <ceylonar.main@gmail.com>
2025-07-18 09:17:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyDdN7HRmZyl284l_vr0sUs-JwTqNrowr1I",
   authDomain: "storeflex-lite.firebaseapp.com",
   projectId: "storeflex-lite",
-  storageBucket: "storeflex-lite.firebasestorage.app",
+  storageBucket: "storeflex-lite.appspot.com",
   messagingSenderId: "1099463811011",
   appId: "1:1099463811011:web:a20b937575eda8dc3ba776"
 };
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
