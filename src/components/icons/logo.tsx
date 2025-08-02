aa951a0aa75606c85459437df446d667a32a7cda
Ceylon Ar <ceylonar.main@gmail.com>
2025-08-02 07:58:01 +0000

    change logo with this https://i.postimg.cc/Qdh5pjHB/Screenshot-2025-08-0

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,7 +5,7 @@ import type { SVGProps } from 'react';
 export function Logo(props: SVGProps<SVGSVGElement> & { className?: string }) {
   return (
     <Image
-      src="https://i.postimg.cc/ZRjxy15t/logo-2.png"
+      src="https://i.postimg.cc/Qdh5pjHB/Screenshot-2025-08-02-132704.png"
       alt="StoreFlex Lite Logo"
       width={40}
       height={40}
