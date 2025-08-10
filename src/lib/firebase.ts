8ed5603134029da6c4115785b9f6ee9af3ede7d9
Ceylon Ar <ceylonar.main@gmail.com>
2025-08-10 11:03:15 +0000

    it same like previous

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCSmSNmAeCTut9VRG19zjdmxKcxRJBHG8o",
+  authDomain: "storeflex-5c94c.firebaseapp.com",
+  projectId: "storeflex-5c94c",
+  storageBucket: "storeflex-5c94c.appspot.com",
+  messagingSenderId: "225594380185",
+  appId: "1:225594380185:web:3ada66fbc40a2f9990cd08"
 };
 
 // Initialize Firebase
